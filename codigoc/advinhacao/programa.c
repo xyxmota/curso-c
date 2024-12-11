8ac39127a28f55bb97c5d3c703f13a90506c26b8
lucasmota_22 <145806163+lucasmota272@users.noreply.github.com>
2024-12-11 20:42:37 -0300

    Primeiro commit com exercícios do curso de C

--- codigoc/advinhacao/programa.c
+++ codigoc/advinhacao/programa.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
 int main() {
-  printf("hello world\n");
+  printf("************************************\n");
+  printf("*******Bem vindo ao programa********\n");
+  printf("************************************\n");
 }
