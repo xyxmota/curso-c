6e763615059ae31ee8662653c025ca5c72b179f9
lucasmota_22 <145806163+lucasmota272@users.noreply.github.com>
2024-12-11 21:21:00 -0300

    adicionei as condicionais

--- codigoc/advinhacao/programa.c
+++ codigoc/advinhacao/programa.c
@@ -3,4 +3,22 @@ int main() {
   printf("************************************\n");
   printf("*******Bem vindo ao programa********\n");
   printf("************************************\n");
+
+  int numerosecreto = 20;
+  int chute;
+
+  printf("qual voce acha que e :");
+  scanf("%d", &chute);
+  printf("o numero e %d ", chute);
+
+  if (chute == numerosecreto) {
+    printf("boa garoto, voce acertou!!\n");
+    printf("jogue de novo, essa foi facil em?");
+  } else {
+    printf("tente de novo meu chefe, tenho fe que na proxima vai!");
+  }
+
+
+
+  // printf("a idade do lucas e %d \n", idade);
 }
