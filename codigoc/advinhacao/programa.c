96c5c8619417dc05a0aaa9d53a140af16a7e4af8
lucasmota_22 <145806163+lucasmota272@users.noreply.github.com>
2024-12-12 23:47:14 -0300

    atualizando a logica desse jogo

--- codigoc/advinhacao/programa.c
+++ codigoc/advinhacao/programa.c
@@ -5,23 +5,35 @@ int main() {
   printf("************************************\n");
 
   int numerosecreto = 20;
+
   int chute;
 
-  printf("qual voce acha que e :");
-  scanf("%d", &chute);
-  printf("o numero que voce chutou e %d. ", chute);
+  for ( int i = 0; i <= 2; i++) {
 
-  if (chute == numerosecreto) {
-    printf("boa garoto, voce acertou!!\n");
-    printf("jogue de novo, essa foi facil em?");
-  } else {
+    printf("Tentativa %d de 3\n", i);
+    printf("Qual e o seu chute? ");
 
-      if (chute > numerosecreto) {
-          printf("Seu chute foi maior que o numero secreto em");
+    scanf("%d", &chute);
+    printf("o numero que voce chutou e %d. ", chute);
+
+    int acertou = (chute == numerosecreto);
+
+    if (acertou) {
+      printf("boa garoto, voce acertou!!\n");
+      printf("jogue de novo, essa foi facil em?\n");
+    } else {
+
+      int maior = chute > numerosecreto;
+      if (maior) {
+        printf("Seu chute foi maior que o numero secreto em");
       }
-      if (chute < numerosecreto) {
-          printf("Seu chute foi menor que o numero secreto em");
+      else {
+        printf("Seu chute foi menor que o numero secreto em");
       }
+    }
   }
+  printf("Fim de Jogo! ");
+  return 0;
 }
-// parei na aula 02.02
+
+// parei na aula 02.06
